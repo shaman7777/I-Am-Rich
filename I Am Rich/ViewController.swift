aa61483f8cd004128835e90eb41cc2a55ea8e4d7
Domanin <148741986+shaman7777@users.noreply.github.com>
2024-10-18 22:12:16 +0300

    Add files via upload

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by zhenya on 18.10.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
